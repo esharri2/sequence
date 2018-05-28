e12f1822302ab79992e9273801ccb3287ca26ca7
esharri2 <esharri2@gmail.com>
2018-05-28 13:47:31 -0400

    added static path

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const router = require("express").Router();
 const userAction = require("./sequences.js");
 const userManagement = require("./users.js");
 
+//TODO testing - remove
 router.use("/", (req, res, next) => {
-    console.log("-------------------------------------------");    
+    console.log("-------------------------------------------");
     console.log(req.headers);
     console.log("-------------------------------------------DERP");
     console.log(req.session.cookie)
@@ -13,5 +14,9 @@ router.use("/", (req, res, next) => {
 
 router.use("/api", userAction);
 router.use("/api", userManagement);
+
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 module.exports = router;
