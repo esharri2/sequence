8b72f6edfa49d55f35889fec566ddcd545368cce
esharri2 <esharri2@gmail.com>
2018-07-01 19:52:31 -0400

    webpack copy fix

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,7 +21,10 @@ const config = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: 'src/index.html' })
+        new HtmlWebpackPlugin({ template: 'src/index.html' }),
+        new CopyWebpackPlugin([
+            { from: 'src/pwa' }           
+          ])
     ],
     mode: "development",
     devServer: {
@@ -40,7 +43,7 @@ if (process.env.NODE_ENV === 'production') {
         }), //Sets process in compiled code
         new UglifyJsPlugin(), //minifies code
         new ManifestPlugin({ filename: 'asset-manifest-json' }), //creates asset manifest for service worker
-        new CopyWebpackPlugin([{ from: 'src/pwa', to: 'dist' }]), //moves pwa stuff to dist file
+        
         new SWPrecacheWebpackPlugin({
             // By default, a cache-busting query parameter is appended to requests
             // used to populate the caches, to ensure the responses are fresh.
