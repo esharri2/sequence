19852f4ad140ec7c8c1bbe693133481e42d6d212
Evan Harrison <esharri2@gmail.com>
2020-03-08 12:25:10 -0400

    update logo

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -3,6 +3,7 @@ import React from "react";
 import Back from "../components/back";
 import Heading from "../components/heading";
 import Layout from "../components/layout";
+import Link from "../components/link";
 import Paragraph from "../components/paragraph";
 
 export default () => (
@@ -13,6 +14,11 @@ export default () => (
       Vois is a talking timer for yoga and exercise. Build a series of actions,
       set a duration for each action, and then listen to the timer guide you
       from one action to the next.
+    </Paragraph>
+    <Heading level={2}>Questions? Ideas?</Heading>
+    <Paragraph>
+      Have a question, idea, or feedback?{" "}
+      <Link to="/contact/">Get in touch here.</Link>
     </Paragraph>
     <Paragraph>
       Vois was created by{" "}
