4b36819605aad4f1d6c727d8ea76439746b5b279
Evan Harrison <esharri2@gmail.com>
2020-10-10 14:37:30 -0400

    fix typo

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 7 * 24 * 60 * 60 * 1000, secure: false },
-    SameSite: "none",
+    sameSite: "none",
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
